091e599aa5fcbe04d687746ca6ec88a2850d12bc
Ivan Krysak <ivan@rev.ng>
2022-05-09 16:38:23 +0300

    Use TTG-based assembly to weaken dependency on efa

--- lib/Yield/Assembly/Plain.cpp
+++ lib/Yield/Assembly/Plain.cpp
@@ -7,12 +7,9 @@
 
 #include "llvm/Support/FormatVariadic.h"
 
-#include "revng/EarlyFunctionAnalysis/FunctionMetadata.h"
 #include "revng/Model/Binary.h"
 #include "revng/Yield/Internal/Function.h"
 #include "revng/Yield/Plain.h"
-
-namespace yield::plain {
 
 static std::string linkAddress(const MetaAddress &Address) {
   std::string Result = Address.toString();
@@ -28,15 +25,15 @@ static std::string linkAddress(const MetaAddress &Address) {
   return Result;
 }
 
-static std::string link(const MetaAddress &Target,
-                        const efa::FunctionMetadata &Metadata,
-                        const model::Binary &Binary) {
+static std::string deduceName(const MetaAddress &Target,
+                              const yield::Function &Function,
+                              const model::Binary &Binary) {
   if (auto Iterator = Binary.Functions.find(Target);
       Iterator != Binary.Functions.end()) {
     // The target is a function
     return Iterator->name().str().str();
-  } else if (auto Iterator = Metadata.ControlFlowGraph.find(Target);
-             Iterator != Metadata.ControlFlowGraph.end()) {
+  } else if (auto Iterator = Function.BasicBlocks.find(Target);
+             Iterator != Function.BasicBlocks.end()) {
     // The target is a basic block
 
     // TODO: maybe there's something better than the address to put here.
@@ -46,49 +43,96 @@ static std::string link(const MetaAddress &Target,
     return "instruction_at_" + linkAddress(Target);
   } else {
     // The target is impossible to deduce, it's an indirect call or the like.
-    return "(unknown)";
+    return "(error)";
   }
 }
 
 static std::string label(const yield::BasicBlock &BasicBlock,
-                         const efa::FunctionMetadata &Metadata,
+                         const yield::Function &Function,
                          const model::Binary &Binary) {
-  if (BasicBlock.Type != yield::BasicBlockType::Ordinary)
-    return "";
-
-  return link(BasicBlock.Address, Metadata, Binary) + ":\n";
+  std::string Result = deduceName(BasicBlock.Address, Function, Binary);
+  return Result += BasicBlock.LabelIndicator + "\n";
 }
 
-static std::string instruction(const yield::Instruction &Instruction) {
-  return Instruction.Raw + '\n';
-}
+static std::string instruction(const yield::Instruction &Instruction,
+                               const yield::BasicBlock &BasicBlock) {
+  std::string Result = Instruction.Raw;
 
-static std::string basicBlock(const yield::BasicBlock &BasicBlock,
-                              const efa::FunctionMetadata &Metadata,
-                              const model::Binary &Binary) {
-  std::string Result;
-
-  Result += label(BasicBlock, Metadata, Binary);
-  for (const auto &Instruction : BasicBlock.Instructions)
-    Result += instruction(Instruction);
+  if (!Instruction.Comment.empty())
+    Result += ' ' + BasicBlock.CommentIndicator + ' ' + Instruction.Comment;
+  else if (!Instruction.Error.empty())
+    Result += ' ' + BasicBlock.CommentIndicator
+              + " Error: " + Instruction.Error;
 
   return Result;
 }
 
-std::string assembly(const yield::BasicBlock &BasicBlock,
-                     const efa::FunctionMetadata &Metadata,
-                     const model::Binary &Binary) {
-  return basicBlock(BasicBlock, Metadata, Binary);
+template<bool ShouldMergeFallthroughTargets>
+static std::string basicBlock(const yield::BasicBlock &BasicBlock,
+                              const yield::Function &Function,
+                              const model::Binary &Binary) {
+  // Blocks are strung together if there's no reason to keep them separate.
+  // This determines whether this is the last block in the current string
+  // (if `NextBlock` is `nullptr`) or if there's continuation.
+  const yield::BasicBlock *NextBlock = nullptr;
+  for (const MetaAddress &Target : BasicBlock.Targets) {
+    if (Target == BasicBlock.NextAddress) {
+      auto Iterator = Function.BasicBlocks.find(Target);
+      revng_assert(Iterator != Function.BasicBlocks.end());
+
+      using namespace yield::BasicBlockType;
+      if (shouldSkip<ShouldMergeFallthroughTargets>(Iterator->Type)) {
+        NextBlock = &*Iterator;
+      }
+    }
+  }
+
+  // String the results together.
+  std::string Result;
+  for (const auto &Instruction : BasicBlock.Instructions)
+    Result += instruction(Instruction, BasicBlock);
+
+  if (NextBlock != nullptr)
+    return Result += basicBlock<ShouldMergeFallthroughTargets>(*NextBlock,
+                                                               Function,
+                                                               Binary);
+  else
+    return Result;
 }
-std::string assembly(const yield::Function &Function,
-                     const efa::FunctionMetadata &Metadata,
-                     const model::Binary &Binary) {
+
+template<bool ShouldMergeFallthroughTargets>
+static std::string basicBlockString(const yield::BasicBlock &BasicBlock,
+                                    const yield::Function &Function,
+                                    const model::Binary &Binary) {
+  // Blocks that are merged into other block strings cannot start a new one.
+  using namespace yield::BasicBlockType;
+  if (shouldSkip<ShouldMergeFallthroughTargets>(BasicBlock.Type))
+    return "";
+
+  return label(BasicBlock, Function, Binary)
+         + basicBlock<ShouldMergeFallthroughTargets>(BasicBlock,
+                                                     Function,
+                                                     Binary);
+}
+
+std::string yield::plain::functionAssembly(const yield::Function &Function,
+                                           const model::Binary &Binary) {
   std::string Result;
 
   for (const auto &BasicBlock : Function.BasicBlocks)
-    Result += basicBlock(BasicBlock, Metadata, Binary);
+    Result += basicBlockString<false>(BasicBlock, Function, Binary);
 
   return Result;
 }
 
-} // namespace yield::plain
+std::string yield::plain::controlFlowNode(const MetaAddress &BasicBlockAddress,
+                                          const yield::Function &Function,
+                                          const model::Binary &Binary) {
+  auto Iterator = Function.BasicBlocks.find(BasicBlockAddress);
+  revng_assert(Iterator != Function.BasicBlocks.end());
+
+  auto Result = basicBlockString<true>(*Iterator, Function, Binary);
+  revng_assert(!Result.empty());
+
+  return Result;
+}
--- lib/Yield/Internal/Verify.cpp
+++ lib/Yield/Internal/Verify.cpp
@@ -11,44 +11,62 @@
 #include "revng/Yield/Internal/Tag.h"
 
 bool yield::Tag::verify(model::VerifyHelper &VH) const {
-  VH.maybeFail(Type != TagType::Invalid);
-  VH.maybeFail(FromPosition < ToPosition);
-  VH.maybeFail(FromPosition != std::string::npos);
-  VH.maybeFail(ToPosition != std::string::npos);
+  if (Type == TagType::Invalid)
+    return VH.fail("The type of this tag is not valid.");
+  if (FromPosition == std::string::npos)
+    return VH.fail("This tag doesn't have a starting point.");
+  if (ToPosition == std::string::npos)
+    return VH.fail("This tag doesn't have an ending point.");
+  if (FromPosition >= ToPosition)
+    return VH.fail("This tag doesn't have a positive length.");
 
   return true;
 }
 
 bool yield::Instruction::verify(model::VerifyHelper &VH) const {
-  VH.maybeFail(Address.isValid());
-  VH.maybeFail(!Raw.empty());
-  VH.maybeFail(!Bytes.empty());
+  if (Address.isInvalid())
+    return VH.fail("An instruction has to have a valid address.");
+  if (Raw.empty())
+    return VH.fail("A raw view of an instruction cannot be empty.");
+  if (Bytes.empty())
+    return VH.fail("An instruction has to be at least one byte big.");
 
   // TODO: Tags might need special verification as well.
+  // Their order, the fact that they don't overlap in undesirable ways, etc.
 
   return true;
 }
 
 bool yield::BasicBlock::verify(model::VerifyHelper &VH) const {
-  VH.maybeFail(Address.isValid());
-  VH.maybeFail(Type != BasicBlockType::Invalid);
+  if (Start.isInvalid())
+    return VH.fail("A basic block has to have a valid start address.");
+  if (End.isInvalid())
+    return VH.fail("A basic block has to have a valid end address.");
+  if (Instructions.empty())
+    return VH.fail("A basic block has to store at least a single instruction.");
 
-  VH.maybeFail(!Instructions.empty());
   for (const auto &Instruction : Instructions)
-    Instruction.verify(VH);
+    if (!Instruction.verify(VH))
+      return VH.fail("Instuction verification failed.");
 
-  VH.maybeFail(!CommentIndicator.empty());
-  VH.maybeFail(!LabelIndicator.empty());
+  if (CommentIndicator.empty())
+    return VH.fail("A basic block has to store a valid comment indicator.");
+  if (LabelIndicator.empty())
+    return VH.fail("A basic block has to store a valid label indicator.");
 
   return true;
 }
 
 bool yield::Function::verify(model::VerifyHelper &VH) const {
-  VH.maybeFail(Address.isValid());
+  if (Entry.isInvalid())
+    return VH.fail("A function has to have a valid entry point.");
 
-  VH.maybeFail(!BasicBlocks.empty());
+  if (BasicBlocks.empty())
+    return VH.fail("A function has to store at least a single basic block.");
+
   for (const auto &BasicBlock : BasicBlocks)
-    BasicBlock.verify(VH);
+    if (!BasicBlock.verify(VH))
+      return VH.fail("Basic block verification failed.");
 
   return true;
 }
