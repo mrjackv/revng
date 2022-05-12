d2efe795501192eaa3aea28bf997cb843652d9f3
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2022-05-12 16:12:18 +0200

    Move globals to subdirectory
    Pipeline globals (specifically model.yml) are better handled if they
    are in a subdirectory. This commit moves the serialization of all
    globals in the "globals" subdirectory.

--- lib/Pipeline/GlobalsMap.cpp
+++ lib/Pipeline/GlobalsMap.cpp
@@ -5,6 +5,7 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include <filesystem>
 #include <system_error>
 
 #include "llvm/ADT/StringRef.h"
@@ -18,19 +19,27 @@ using namespace pipeline;
 using namespace llvm;
 
 llvm::Error GlobalsMap::storeToDisk(llvm::StringRef Path) const {
-  for (const auto &Global : Map)
-    if (auto E = Global.second->storeToDisk(Path.str() + "/"
-                                            + Global.first().str());
-        !!E)
+  for (const auto &Global : Map) {
+    std::filesystem::path Root(Path.str());
+    std::filesystem::path ContextDir = Root / "context";
+    if (auto EC = llvm::sys::fs::create_directories(std::string(ContextDir));
+        EC)
+      return llvm::createStringError(EC,
+                                     "Could not create dir %s",
+                                     ContextDir.c_str());
+    auto FilePath = Root / "context" / Global.first().str();
+    if (auto E = Global.second->storeToDisk(std::string(FilePath)); !!E)
       return E;
+  }
   return llvm::Error::success();
 }
 
 llvm::Error GlobalsMap::loadFromDisk(llvm::StringRef Path) {
-  for (const auto &Global : Map)
-    if (auto E = Global.second->loadFromDisk(Path.str() + "/"
-                                             + Global.first().str());
-        !!E)
+  for (const auto &Global : Map) {
+    std::filesystem::path Root(Path.str());
+    auto FilePath = Root / "context" / Global.first().str();
+    if (auto E = Global.second->loadFromDisk(std::string(FilePath)); !!E)
       return E;
+  }
   return llvm::Error::success();
 }
